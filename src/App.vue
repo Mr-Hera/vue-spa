5c1c15eaa616f31e8df7ae2988d4e3e646881220
Mr-Hera <randellhera@gmail.com>
2023-08-04 15:21:13 +0300

    Feat: added input form & dynamic submit button on successful form input

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,27 @@
       :nav-link-click="(index) => activePage = index"
     ></navbar>
     
-    <page-viewer
+    <!-- <page-viewer
         v-if="pages.length > 0"
         :page="pages[activePage]"
-    ></page-viewer>
+    ></page-viewer> -->
+
+    <create-page
+      :page-created="pageCreated"
+    ></create-page>
 </template>
 
 
 <script>
     import Navbar from "./components/Navbar.vue";
     import PageViewer from "./components/PageViewer.vue";
+    import CreatePage from "./components/CreatePage.vue";
 
     export default {
         components: {
             Navbar,
-            PageViewer
+            PageViewer,
+            CreatePage
         },
         created () {
             this.getPages();
@@ -36,6 +42,9 @@
                 let data = await res.json();
 
                 return this.pages = data;
+            },
+            pageCreated(pageObj) {
+                console.log(pageObj);
             }
         }
     }

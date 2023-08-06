3a5c1fe12fa2a39963368b27d57097405903ed03
Mr-Hera <randellhera@gmail.com>
2023-08-07 01:18:53 +0300

    Feat: introduction to composition API in contrast to options API

---
+++ src/views/PagesList.vue
@@ -0,0 +1,30 @@
+<template>
+    <p>{{ data.counter }}</p>
+    <button @click.prevent="click">Increment</button>
+</template>
+
+<script setup>
+    import { ref, reactive } from 'vue';
+    // const counter = ref(0);
+    const data = reactive({ counter: 0 });
+
+    function click() {
+        data.counter++;
+    }
+    // export default {
+    //     data() {
+    //         return {
+    //             counter: 0
+    //         }
+    //     },
+    //     methods: {
+    //         click() {
+    //             this.counter++
+    //         }
+    //     }
+    // }
+</script>
+
+<style>
+
+</style>
